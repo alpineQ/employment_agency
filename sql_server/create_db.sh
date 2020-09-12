b37872d8436c4846e7c0436d76b748c9a80ac00e
alpineQ <maxk_dev@mail.ru>
2020-09-12 23:08:15 +0300

    Небольшие исправления

--- sql_server/create_db.sh
+++ sql_server/create_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sleep 18s
+sleep 25s
 echo ----------------------- CREATING DATABASE ... ------------------------------------------
 /opt/mssql-tools/bin/sqlcmd -S localhost,1433 -i /usr/src/app/CreateDB.sql -U SA -P QwErTy123!
